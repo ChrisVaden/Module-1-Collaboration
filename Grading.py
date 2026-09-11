711af249ad6be349ae354e845ed7445beb1457cc
ChrisVaden <cvaden9@ivytech.edu>
2026-09-11 14:46:11 -0400

    Add files via upload

---
+++ Grading.py
@@ -0,0 +1,13 @@
+
+grade = int(input("What is your grade?: "))
+
+if grade >= 90:
+    print("Your letter grade is A")
+elif grade >= 80:
+    print("Your letter grade is B")
+elif grade >= 70:
+    print("Your letter grade is C")
+elif grade >= 60:
+    print("Your letter grade is D")
+elif grade <= 60:
+    print("Your letter grade is F")

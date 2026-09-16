efca11816458cb977c3cb83eface6150ce993ee6
ChrisVaden <cvaden9@ivytech.edu>
2026-09-15 23:47:12 -0400

    Add files via upload

---
+++ 8.4-9.14.py
@@ -0,0 +1,57 @@
+# 8.4
+years_list = [2006, 2007, 2008, 2009, 2010, 2011]
+#8.5, 8.6
+# things_list = ["mozzarella", "cinderella", "salmonell"]
+
+# things_list[2] = things_list[2].capitalize()
+
+# print(things_list)
+
+# thing_list = ["Mozzarella", "Cinderella", "Salmonell"]
+
+# thing_list[2] = thing_list[2].capitalize()
+
+# print(thing_list)
+
+# 8.7
+#things.remove("salmonella")  # or: del things[2]
+# print(things)
+
+# 8.10
+# even = [num for num in range(10) if num % 2 == 0]
+# print(even)
+
+# 9.6 
+# life = {
+#     'animals': {
+#         'cats': ['Henri', 'Grumpy', 'Lucy'],
+#         'octopi': {},
+#         'emus': {}
+#     },
+#     'plants': {},
+#     'other': {}
+# }
+
+# 9.7 
+# print(list(life.keys()))
+
+# 9.8 
+# print(list(life['animals'].keys()))
+
+# #.9 
+# print(life['animals']['cats'])
+
+# 9.10
+# squares = {num: num ** 2 for num in range(10)}
+# print(squares)
+
+# 9.11 
+# odd = {num for num in range(10) if num % 2 != 0}
+# print(odd)
+
+# 9.14 
+# titles = ['Creature of Habit', 'Crewel Fate', 'Sharks On a Plane']
+# plots = ['A nun turns into a monster', 'A haunted yarn shop', 'Check your exits']
+
+# movies = dict(zip(titles, plots))
+# print(movies)
